16b174fa166b74500b32368c3d10b9eb460da197
rakeshymts <107837608+rakeshymts@users.noreply.github.com>
2022-11-30 23:57:35 +0530

    Quote generator

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Print data..");
+// let apiQuotes = [];
+// // Get Quotes from API
+// async function getQuotes() {
+//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//     try {
+//         alert('true');
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         console.log(apiQuotes);
+//     } catch (error) {
+//         alert('false');
+//         //Catch Error Here
+//     }
+// }
+
+// // On Load
+// getQuotes();
